0dd96badd7279ccfaf883e5a1ade1bb4d585881a
firasaj <firas.aj@icloud.com>
2024-08-12 11:47:48 -0400

    test if commit works

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
+import Link from "next/link"
 
 export default function Home() {
   return (
-    <div className="items-center flex-col">
+    <div className="flex-col flex items-center">
       <h1 className = "text-7xl font-bold mt-16 py-4 text-center">Sage</h1>
-      <a href="/api/auth/login">Login</a>
+      <Link href="/api/auth/login">
+        <button className="py-4 px-6 mt-16 bg-black rounded-xl hover:bg-slate-500 text-white ease-in-out duration-300">Login</button>
+      </Link>
     </div>
     
   );
